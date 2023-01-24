271fc28e9b922b5a4f6d9820470f5368fe9e6d02
Mu-een <mueenslam@gmail.com>
2023-01-24 16:57:05 +0200

    1st commit

---
+++ src/components/AboutComp.vue
@@ -0,0 +1,88 @@
+<template>
+    <div>
+    <h1>How To Budget!</h1>
+    <p>Most people need some way of seeing where their money is going each month. A budget can help you feel more in control of your finances and make it easier to save money for your goals.
+</p>
+
+<h2>Don't ask how to budget money, ask why you want to budget</h2>
+<p>The key to success in any endeavor is to create specific, yet challenging objectives. Understanding what motivates you to save can go a long way toward creating clear, achievable savings targets. And keeping your savings goals in mind can help you to stay focused and on track, even when things aren't so easy.</p>
+
+<h2>Distinguish between short-term savings goals and long-term saving goals</h2>
+<p>What is a short-term savings goal? These relatively modest targets may include the following:</p>
+<ul>
+    <li>A nice piece of furniture</li>
+    <li>A weekend vacation</li>
+    <li>A down payment on a car</li>
+    <li>An emergency fund</li>
+</ul>
+
+<p>Long-term savings goals might look like:</p>
+<ul>
+    <li>A deposit for a flat or a house</li>
+    <li>Paying off any long-standing debt</li>
+    <li>Starting up your own business</li>
+    <li>A trip around the world</li>
+    <li>Saving for retirement</li>
+</ul>
+<p>Having a combination of both short- and long-term saving goals can make the larger goals seem less intimidating. As you gradually achieve your short-term goals, you may realize how possible it is to save money and be in control of your finances. And this can make your long-term goals feel increasingly attainable.</p>
+
+<h2>Track your spending to create a solid budget</h2>
+<p>If you want to start budgeting effectively, you need to understand exactly how much is coming in and out of your account every month.
+The best way to do this is to track all of your income and expenses over a 30-day period. This means being aware of every single transaction and either noting it down in a spreadsheet or using our budgeting app. </p>
+
+<h2>Separate fixed expenses from variable expenses</h2>
+<p>The category of fixed costs may include:</p>
+<ul>
+    <li>Monthly rent</li>
+    <li>Heating and electricity bills</li>
+    <li>Insurance costs (i.e. car, personal liability or property)</li>
+    <li>Student loan repayments</li>
+</ul>
+
+<p>Variable costs, which may change monthly or even weekly, include:</p>
+<ul>
+    <li>Grocery shopping</li>
+    <li>Entertainment (i.e. nights out, cinema trips, concerts)</li>
+    <li>Clothes shopping</li>
+    <li>Eating out</li>
+</ul>
+
+<h2>Plan a monthly budget</h2>
+<p>So, you've worked out why you want to budget, what you want to save for, and what your fixed and variable costs are. Now it's time to work out how to save money each month.</p>
+<p>The 50/30/20 rule encourages your budget to look as follows:</p>
+<ul>
+    <li>50% of your income goes towards your “needs,” i.e. your fixed costs such as rent and bills.</li>
+    <li>30% is allocated to your “wants,” i.e. your variable costs such as eating out, trips to the hairdresser and clothes shopping.</li>
+    <li>20% goes into your savings or towards paying off debt.
+</li><br>
+
+<p>The 50/30/20 rule was actually created by US senator Elizabeth Warren, a bankruptcy specialist at Harvard, as a way to show American citizens how to budget and save money and how to budget money on a low income.</p>
+</ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'AboutComp'
+    }
+</script>
+
+<style scoped>
+template{
+    background-color: #ccd0d8;
+}
+
+h1 ,h2{
+    color: #ff4200;
+    text-shadow: 2px 3px 3px #ff9001;
+}
+
+p {
+    color:#1d2023;
+    font-size: 17px;
+}
+
+li {
+    font-size: 17px;
+}
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+  <AboutComp />
+  </div>
+</template>
+
+<script>
+import AboutComp from '@/components/AboutComp.vue'
+  export default {
+    name: 'AboutView',
+    components: {
+      AboutComp
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
